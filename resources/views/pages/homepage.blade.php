89898153a7774ff3567b134a42a2759a214f5968
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2021-09-17 15:14:06 +0200

    1c Beginnetje aangemaakt voor styling

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -7,7 +7,8 @@
 @endsection
 
 @section('content')
-
+<!-- Nieuwe div voor styling op lijsten in homepagina (Ticket 1c) -->
+<div class="list">
     <h1>
         @section('title')
             {{ __('misc.all_brands') }}
@@ -22,40 +23,43 @@
     ?>
 
     <div class="container">
+        
         <!-- Example row of columns -->
-        <div class="row">
+            <div class="row">
 
-            @foreach($brands->chunk($chunk_size) as $chunk)
-                <div class="col-md-4">
+                @foreach($brands->chunk($chunk_size) as $chunk)
+                    <div class="col-md-4">
 
-                    <ul>
-                        @foreach($chunk as $brand)
+                        <ul>
+                            @foreach($chunk as $brand)
 
-                            <?php
-                            $current_first_letter = strtoupper(substr($brand->name, 0, 1));
+                                <?php
+                                $current_first_letter = strtoupper(substr($brand->name, 0, 1));
 
-                            if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
-                                echo '</ul>
-						<h2>' . $current_first_letter . '</h2>
-						<ul>';
-                            }
-                            $header_first_letter = $current_first_letter
-                            ?>
+                                if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
+                                    echo '</ul>
+                            <h2>' . $current_first_letter . '</h2>
+                            <ul>';
+                                }
+                                $header_first_letter = $current_first_letter
+                                ?>
+                                    <li>
+                                        <!-- Links krijgen een nieuwe styling (Ticket 1c) -->
+                                        <a class="list-button" href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/">{{ $brand->name }}</a>
+                                    </li>
+                                
+                            @endforeach
+                        </ul>
 
-                            <li>
-                                <a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/">{{ $brand->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    </div>
+                    <?php
+                    unset($header_first_letter);
+                    ?>
+                @endforeach
 
-                </div>
-                <?php
-                unset($header_first_letter);
-                ?>
-            @endforeach
-
-        </div>
+            </div>
+        
 
     </div>
-
+</div>
 @endsection
