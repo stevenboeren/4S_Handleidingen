2ab27a4a2e863ca13662efd4d18ec22d72f70422
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2021-09-22 16:18:27 +0200

    Fixed #11 Ticket 2c Link gestyled als knop.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,13 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
     <div class="container">
-        <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
-            <h1>{{ __('misc.homepage_title') }}</h1>
-        </a>
+        <!-- Ticket 2c: Class toegevoegd aan link  -->
+        <div class="button-manual">
+            <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
+                <h1>{{ __('misc.homepage_title') }}</h1>
+            </a>
+        </div>
+        
         @yield('introduction_text')
     </div>
 </div>
